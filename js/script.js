51b5ee3343e34e556ad15292b579d0ca2dc1bbf4
Kamil Snietka <krevelir@gmail.com>
2022-12-26 17:41:22 +0100

    Tidied up variables

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 {
-    const toggleTheme = () => {
+    const toggleBodyClass = () => {
         const body = document.querySelector(".body");
-        const themeName = document.querySelector(".themeName")
-
         body.classList.toggle("lightTheme");
+    }
+
+    const toggleThemeName = () => {
+        const themeName = document.querySelector(".themeName");
         themeName.innerText = body.classList.contains("lightTheme") ? "Dark" : "Light";
+    }
+
+    const toggleTheme = () => {
+        toggleBodyClass();
+        toggleThemeName();
+
     };
 
     const init = () => {
         const buttonThemeSwitch = document.querySelector(".section__button");
         buttonThemeSwitch.addEventListener("click", toggleTheme)
     }
-    
+
     init();
-    
+
 }
